e73c803a0fe4ec5f91049dcfc0f0ce81397364f4
John-droid-dotcom <83913756+John-droid-dotcom@users.noreply.github.com>
2022-01-25 22:37:32 +0300

    improved UI the components

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 "use strict";
-var parent = document.querySelector(".btn");
-parent.addEventListener("mouseover", function() {
-    document.selectElelementByClass("icon-img").style.filter =
-        "invert(100%) sepia(0%) saturate(2%) hue-rotate(212deg) brightness(104%) contrast(101%);";
-});
-parent.addEventListener("mouseout", function() {
-    parent.children[0].style.filter =
-        "invert(20%) sepia(29%) saturate(7498%) hue-rotate(340deg) brightness(88%) contrast(97%)";
-});
+// var parent = document.querySelector(".btn");
+// parent.addEventListener("mouseover", function () {
+//   document.getElelementByClass("icon-img").style.filter =
+//     "invert(100%) sepia(0%) saturate(2%) hue-rotate(212deg) brightness(104%) contrast(101%);";
+// });
+// parent.addEventListener("mouseout", function () {
+//   parent.children[0].style.filter =
+//     "invert(20%) sepia(29%) saturate(7498%) hue-rotate(340deg) brightness(88%) contrast(97%)";
+// });
