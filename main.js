0651233f00b7e41a053b1f64e9019536a9f95d6f
yohannesakd <yoniassefayoni@gmail.com>
2022-03-04 00:29:52 +0300

    add testimonials, hotels and restaurants sections

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict";
 const myNav = document.querySelector(".nav-bar");
+const logoFirst = document.querySelector(".logo > svg .cls-1");
+
 window.onscroll = function () {
   if (
     document.body.scrollTop >= 50 ||
@@ -7,8 +9,10 @@ window.onscroll = function () {
   ) {
     myNav.classList.add("nav-colored");
     myNav.classList.remove("nav-transparent");
+    // logoFirst.style.fill = "white";
   } else {
     myNav.classList.add("nav-transparent");
     myNav.classList.remove("nav-colored");
+    // logoFirst.style.fill = "black";
   }
 };
