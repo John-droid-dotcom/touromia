813a90eb572fc2046635c72601d65827a7df503c
yohannesakd <yoniassefayoni@gmail.com>
2022-03-03 15:32:25 +0300

    add packages and stats section

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-var myNav = document.querySelector("fix-nav");
-window.onscroll = function() {
-    "use strict";
-    if (document.body.scrollTop >= 200) {
-        myNav.classList.add("nav-colored");
-        myNav.classList.remove("nav-transparent");
-    } else {
-        myNav.classList.add("nav-transparent");
-        myNav.classList.remove("nav-colored");
-    }
+"use strict";
+const myNav = document.querySelector(".nav-bar");
+window.onscroll = function () {
+  if (
+    document.body.scrollTop >= 50 ||
+    document.documentElement.scrollTop >= 50
+  ) {
+    myNav.classList.add("nav-colored");
+    myNav.classList.remove("nav-transparent");
+  } else {
+    myNav.classList.add("nav-transparent");
+    myNav.classList.remove("nav-colored");
+  }
 };
